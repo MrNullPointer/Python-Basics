68ed384dd2b76fb922367c42e1811764c43740d6
iparikshitdubey <58353903+iparikshitdubey@users.noreply.github.com>
2020-02-01 15:50:24 -0500

    Add files via upload

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,5 +1,44 @@
-x = 5
 
-while x < 100:
-    print (x)
-    x += 1
+#iteration = 0
+
+#while iteration < 5:
+#    for i in "hello":
+#        if iteration%2 == 0:
+#            break
+#    iteration += 1
+
+#******Find the cube root using guesses****#
+
+#cube = int (input("Please enter a number to find the cube root!!: "))
+#guess = 0.0
+#increment = 0.0001
+#guessNumber = 0
+#proximity = 0.001
+
+#while abs(cube - guess**3) > proximity:
+#    guess += increment
+#    guessNumber += 1
+
+#print("The number guessed is :", str(guess))
+
+#if abs(guess**3 - cube) >= proximity:
+#    print("Failed!!", "Number of iterations are: ", str(guessNumber))
+#else: print ("The cube root of", str(cube), "is close to:", str(guess))
+
+
+#******Find the square root using guesses****#
+x = 25
+epsilon = 0.01
+step = 0.1
+guess = 0.0
+
+while guess <= x:
+    if abs(guess**2 -x) < epsilon:
+        break
+    else:
+        guess += step
+
+if abs(guess**2 - x) >= epsilon:
+    print('failed')
+else:
+    print('succeeded: ' + str(guess))
